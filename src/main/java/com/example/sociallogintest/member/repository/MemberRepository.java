a8f6b7c5b3be3bab696641f61f59d8ce907bfbc5
shim tae hoon <sth2721@gmail.com>
2024-10-24 15:12:33 +0900

    1024 ddd

--- src/main/java/com/example/sociallogintest/member/repository/MemberRepository.java
+++ src/main/java/com/example/sociallogintest/member/repository/MemberRepository.java
@@ -3,24 +3,21 @@ package com.example.sociallogintest.member.repository;
 import com.example.sociallogintest.member.entity.Member;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, String> {
 
-    @Modifying
-    @Query("DELETE FROM Member m WHERE m.id = :id")
-    void deleteById(@Param("id") String id);
+//    @Modifying
+//    @Query("DELETE FROM Member m WHERE m.id = :id")
+//    void deleteById(@Param("id") String id);
 
-    @Query("select m from Member m where m.id = :id")
-    Optional<Member> selectById(@Param("id") String id);
+//    @Query("select m from Member m where m.id = :id")
+//    Optional<Member> selectById(@Param("id") String id);
 
     @EntityGraph(attributePaths = {"roleSet"}, type = EntityGraph.EntityGraphType.LOAD)
     @Query("select m from Member m where m.fromSocial = :social and m.email =:email")
     Optional<Member> findByEmail(String email, boolean social);
-
 
 }
